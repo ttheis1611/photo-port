335ab7195704db7222fc10b8c526ce76919a73e4
Terry Theis <tltheis@prodigy.net>
2021-07-03 14:25:24 -0500

    gallery

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 
+
 const PhotoList = ({ category }) => {
 
   const [photos] = useState([
@@ -107,7 +108,8 @@ const PhotoList = ({ category }) => {
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
-            src={require(`../../assets/small/${category}/${i}.jpg`)}
+            //src={require('assets/small}
+            src={require(`../../assets/small/${category}/${i}.jpg`).default}
             alt={image.name}
             className="img-thumbnail mx-1"
             key={image.name}
